d555da9669678b07d7c9abd0afe1b402e57283c6
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-10-30 00:49:08 +0900

    汎用ビューを使用

--- polls/views.py
+++ polls/views.py
@@ -4,8 +4,17 @@ from django.urls import reverse
 
 from django.shortcuts import render, get_object_or_404
 
+from django.views import generic
+
 from .models import Question, Choice
 
+class IndexView (generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'questions'
+
+    def get_queryset(self):
+        return Question.objects.all()
+"""
 def index(request):
     context = {
         'questions': Question.objects.all(),
@@ -13,7 +22,12 @@ def index(request):
 #    template = loader.get_template('polls/index.html')
 #    return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
+"""
+class DetailView (generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
+"""
 def detail(request, question_id):
     try:
         question = Question.objects.get(id=question_id)
@@ -26,7 +40,12 @@ def detail(request, question_id):
     }
     template = loader.get_template('polls/detail.html')
     return HttpResponse(template.render(context, request))
+"""
+class ResultsView (generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
+"""
 def results(request, question_id):
     try:
         question = Question.objects.get(id=question_id)
@@ -39,6 +58,7 @@ def results(request, question_id):
     }
     template = loader.get_template('polls/results.html')
     return HttpResponse(template.render(context, request))
+"""
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
