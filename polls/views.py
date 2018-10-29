422162cada145284cc2c823dcb96b0a7d6745c2b
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2018-10-29 23:48:07 +0900

    detailsページの作成

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from django.shortcuts import render
@@ -7,14 +7,24 @@ from .models import Question
 
 def index(request):
     context = {
-        'questions': Question.objects.order_by('-pub_date'),
+        'questions': Question.objects.all(),
     }
 #    template = loader.get_template('polls/index.html')
 #    return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(id=question_id)
+        choices = question.choice_set.all()
+    except:
+        raise Http404("Questionは存在しません")
+    context = {
+        'question': question,
+        'choices': choices
+    }
+    template = loader.get_template('polls/detail.html')
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
